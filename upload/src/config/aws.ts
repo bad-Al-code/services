85ffaa83679a6ff8214fba97d1256c10c33e687c
Badal <badalsingh120403@gmail.com>
2025-03-09 18:19:29 +0530

    refactor(upload): rename s3 client variable and set AWS region
    - This commit renames the S3 client variable from 's3' to 's3Client' for
    improved clarity and consistency.
    - It also explicitly sets the AWS region in the S3Client configuration
    using the environment variable.

--- upload/src/config/aws.ts
+++ upload/src/config/aws.ts
@@ -2,7 +2,8 @@ import { S3Client } from '@aws-sdk/client-s3';
 
 import { envVariables } from './env';
 
-export const s3 = new S3Client({
+export const s3Client = new S3Client({
+    region: envVariables.AWS_REGION,
     credentials: {
         accessKeyId: envVariables.AWS_ACCESS_KEY_ID,
         secretAccessKey: envVariables.AWS_SECRET_ACCESS_KEY,
