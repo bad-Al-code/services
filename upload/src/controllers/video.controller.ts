03a9e503a4312a3ac93468f3d8525310bc37b92c
Badal <badalsingh120403@gmail.com>
2025-03-10 10:46:12 +0530

    fix(upload): fix s3Url typo

--- upload/src/controllers/video.controller.ts
+++ upload/src/controllers/video.controller.ts
@@ -1,25 +1,40 @@
 import { Request, Response } from 'express';
 import httpSatus from 'http-status-codes';
+import { randomUUID } from 'node:crypto';
 import { z } from 'zod';
+
+import { videos } from '../db/schema';
+import { db } from '../db';
 
 const userIdSchema = z.string().uuid();
 
-export const uploadVideoController = (req: Request, res: Response) => {
-    const { userId } = req.params;
-
+export const uploadVideoController = async (req: Request, res: Response) => {
     try {
+        const { userId } = req.params;
         userIdSchema.parse(userId);
+
+        const s3Url = (req as any).s3Url;
+        const filename = (req as any).file.originalname;
+
+        const videoId = randomUUID();
+
+        await db.insert(videos).values({
+            id: videoId,
+            userId,
+            filename,
+            s3Url,
+        });
+
+        res.status(httpSatus.CREATED).json({
+            message: 'Video uploaded successfully',
+            s3Url,
+            userId,
+            videoId,
+        });
+        return;
     } catch (error) {
+        console.error('Error in uploadVideoController:', error);
         res.status(httpSatus.BAD_REQUEST).json({ error: 'Invalid user ID' });
         return;
     }
-
-    const s3Url = (req as any).s3url;
-
-    res.status(httpSatus.CREATED).json({
-        message: 'Video uploaded successfully',
-        s3Url: s3Url,
-        userId: userId,
-    });
-    return;
 };
