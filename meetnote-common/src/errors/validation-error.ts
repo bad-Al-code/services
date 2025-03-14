8ef3f1220dd09c45ced7b40859c02d22eefceea3
Badal <badalsingh120403@gmail.com>
2025-03-14 16:25:57 +0530

    feat: use zod for validation

--- meetnote-common/src/errors/validation-error.ts
+++ meetnote-common/src/errors/validation-error.ts
@@ -1,18 +1,27 @@
 import { StatusCodes } from 'http-status-codes';
 import { CustomError } from './custom-error';
+import { ZodError, ZodIssue } from 'zod';
 
 export class ValidationError extends CustomError {
   statusCode: StatusCodes = StatusCodes.BAD_REQUEST;
 
-  constructor(public errors: { message: string; field: string }[]) {
+  constructor(public errors: { message: string; field?: string }[]) {
     super('Invalid request parameters');
 
     Object.setPrototypeOf(this, ValidationError.prototype);
   }
 
-  serializeErrors(): { message: string; field?: string }[] {
-    return this.errors.map((err) => {
-      return { message: err.message, field: err.field };
+  static fromZodError(zodError: ZodError): ValidationError {
+    const errors: { message: string; fields?: string }[] = zodError.errors.map((issue: ZodIssue) => {
+      const field = issue.path.length > 0 ? issue.path[issue.path.length - 1].toString() : undefined;
+
+      return { message: issue.message, field };
     });
+
+    return new ValidationError(errors);
+  }
+
+  serializeErrors(): { message: string; field?: string }[] {
+    return this.errors;
   }
 }
