647396a09eba144db472c295ed3333415a76ec63
Badal <badalsingh120403@gmail.com>
2025-03-15 19:23:54 +0530

    fix(auth): use proper dialect and remove ssl

--- auth-service/drizzle.config.ts
+++ auth-service/drizzle.config.ts
@@ -3,15 +3,12 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   out: './src/drizzle/migrations',
   schema: './src/drizzle/schema.ts',
-  dialect: 'postgresql',
-  strict: true,
-  verbose: true,
+  dialect: 'mysql',
   dbCredentials: {
     password: process.env.MYSQL_PASSWORD!,
     user: process.env.MYSQL_USER!,
     database: process.env.MYSQL_DATABASE!,
     host: process.env.MYSQL_HOST!,
     port: Number(process.env.MYSQL_PORT!) || 3306,
-    ssl: false,
   },
 });
